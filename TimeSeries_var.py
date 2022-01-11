6baac352220f058e2209b7a04297ea1ee9a1449c
Serge <97500773+SergPod@users.noreply.github.com>
2022-01-11 19:52:54 +0700

    Add files via upload

---
+++ TimeSeries_var.py
@@ -0,0 +1,14 @@
+DataFile="E:/TMP_PDF/18-08-04/IMG_20180804_180835.jpg"
+DataTime=[0]
+DarkFile=""
+DarkTime=[]
+BaseFile=""
+BaseTime=[]
+SFile="E:/TMP_PDF/18-08-04/IMG_20180804_180835.jpg"
+STime=[0]
+RotA=0.0
+DoCrop=False
+# CropRect provides [Top, Bottom, Left, Right] of rectangle
+CropRect=[0, 0, 0, 0]
+# SRect= [Top, Bottom, Left, Right]
+SRect=[0, 0, 0, 0]
